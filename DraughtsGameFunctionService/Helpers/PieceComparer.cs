2aa02bdbfdf873cf549c736885df91b128a78707
OllieRumbol <otgb97@gmail.com>
2021-03-24 22:33:09 +0000

    new helper, comparer

---
+++ DraughtsGameFunctionService/Helpers/PieceComparer.cs
@@ -0,0 +1,20 @@
+﻿using DraughtsGameFunctionModels.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DraughtsGameFunctionService.Helpers
+{
+    public class PieceComparer : IEqualityComparer<Piece>
+    {
+        public bool Equals(Piece piece1, Piece piece2)
+        {
+            return piece1.Height == piece2.Height && piece1.Width == piece2.Width;
+        }
+
+        public int GetHashCode(Piece piece)
+        {
+            return piece.Height.GetHashCode() + piece.Width.GetHashCode();
+        }
+    }
+}
