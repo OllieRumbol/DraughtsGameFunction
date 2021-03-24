e40cbe4cc3fa0ad1d1506de0cb17c1d8d436c111
OllieRumbol <otgb97@gmail.com>
2021-03-24 22:03:34 +0000

    New endpoint

---
+++ DraughtsGameFunction/PlayerTips.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using DraughtsGameFunctionModels.Controller;
+using Newtonsoft.Json.Linq;
+using DraughtsGameFunctionService.Interface;
+using DraughtsGameFunctionService.Instance;
+using System.Collections.Generic;
+using DraughtsGameFunctionModels.Service;
+
+namespace DraughtsGameFunction
+{
+    public static class PlayerTips
+    {
+        [FunctionName("GetPotentialMoves")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                GetPlayerTips getNextMove = JObject.Parse(requestBody).ToObject<GetPlayerTips>();
+
+                IPlayerTipsService service = new PlayerTipsService();
+                List<Piece> potentialMoves = service.GetPotentialMoves(getNextMove);
+
+                return new OkObjectResult(
+                    new PlayersTipsResponse
+                    {
+                        Successful = true,
+                        PotentialMoves = potentialMoves
+                    }
+                );
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(
+                    new PlayersTipsResponse
+                    {
+                        Successful = false,
+                        ErrorMessage = ex.Message
+                    }
+                );
+            }
+        }
+    }
+}
