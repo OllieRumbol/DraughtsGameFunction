3cc3f2c51e0d8346424de2cf9461a659fa51008b
OllieRumbol <otgb97@gmail.com>
2021-03-24 22:37:51 +0000

    Player tips service

---
+++ DraughtsGameFunctionService/Interface/IPlayerTipsService.cs
@@ -0,0 +1,13 @@
+﻿using DraughtsGameFunctionModels.Controller;
+using DraughtsGameFunctionModels.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DraughtsGameFunctionService.Interface
+{
+    public interface IPlayerTipsService
+    {
+        List<Piece> GetPotentialMoves(GetPlayerTips getPlayerTips);
+    }
+}
