2afbf9ec62750ec32002ac65363cea98cf99d109
OllieRumbol <otgb97@gmail.com>
2021-03-24 22:32:31 +0000

    New models

---
+++ DraughtsGameFunctionModels/Controller/GetPlayerTips.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DraughtsGameFunctionModels.Controller
+{
+    public class GetPlayerTips
+    {
+        public int[,] Board { get; set; }
+
+        public int TipFor { get; set; }
+    }
+}
