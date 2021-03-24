2f9fc04ccda2a75889bb8b5303b7a12bb4196709
OllieRumbol <otgb97@gmail.com>
2021-03-24 22:36:29 +0000

    moving model

---
+++ DraughtsGameFunctionModels/Service/TreeTake.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DraughtsGameFunctionModels.Service
+{
+    public class TreeTake
+    {
+        public int CurrentHeight { get; set; }
+
+        public int CurrentWidth { get; set; }
+
+        public int TakeHeight { get; set; }
+
+        public int TakeWidth { get; set; }
+
+        public int NextHeight { get; set; }
+
+        public int NextWidth { get; set; }
+
+        public int Piece { get; set; }
+    }
+}
